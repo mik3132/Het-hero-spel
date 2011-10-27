c385e07709a59619c7d413ea82d42c43dad3dc04
Edo Freriks <edofre@gmail.com>
2011-10-27 13:42:20 +0200

    enemies left weggehaald

--- src/view/ControlPanel.java
+++ src/view/ControlPanel.java
@@ -30,11 +30,9 @@ public class ControlPanel extends JPanel
 	/** Buttons for the control panel */
 	public JButton bUp,bDown,bLeft,bRight,bShoot, newGame, bexit;
 	/** Labels for the control panel */
-	public JLabel jLabel1, jLabel2;
-	/** TextFields for the control panel */
-	public JTextField  jTextField2;
+	public JLabel jLabel2;
 	
-	public ScoresView jTextField1;
+	public ScoresView scoresView;
 	/** The HeroModel of the current Hero */
 	private HeroModel heroModel;
 	/** the Points */
@@ -56,17 +54,10 @@ public class ControlPanel extends JPanel
 		bShoot = new JButton("FIRE");
 		newGame = new JButton("New game");
 		bexit = new JButton("Exit");
-        jLabel1 = new JLabel("Enemies left");
-        
-        //add scores
-        scoreFields = new ArrayList<JTextField>(); 
         
         jLabel2 = new JLabel("Points left");
-        //jTextField1 = new JTextField(""+500);
-        jTextField1 = new ScoresView(heroModel.scs);
-        jTextField2 = new JTextField("");
-        jTextField1.setFocusable(false);
-        jTextField2.setFocusable(false);
+        scoresView = new ScoresView(heroModel.scs);
+        scoresView.setFocusable(false);
 		
 		this.setLayout(new BorderLayout());
 		this.setSize(300, 800);
@@ -98,12 +89,10 @@ public class ControlPanel extends JPanel
                             .addComponent(bRight, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
                             .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel2)
-                                .addComponent(jLabel1))
+                                .addComponent(jLabel2))
                             .addGap(18, 18, 18)
                             .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(jTextField1, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
-                                .addComponent(jTextField2, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE))))
+                                .addComponent(scoresView, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE))))
                     .addGap(28, 28, 28))
             );
         
@@ -112,12 +101,8 @@ public class ControlPanel extends JPanel
                 .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addGap(36, 36, 36)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel1)
-                        .addComponent(jTextField2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(jLabel2)
-                        .addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                        .addComponent(scoresView, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
                     .addComponent(bUp, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
@@ -174,9 +159,7 @@ public class ControlPanel extends JPanel
 	 */
 	public void update ()
 	{
-		//jTextField1.setText(""+500);
-		jTextField1.update();
-		jTextField2.setText(""); // kan weg
+		scoresView.update();
 	}
 	
 
